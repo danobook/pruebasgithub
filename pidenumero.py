49248976025df861107a0d1ef7cb18451e914b1f
daniel <danobook@gmail.com>
2021-07-08 17:50:13 -0500

    comentario

---
+++ pidenumero.py
@@ -0,0 +1,5 @@
+#Se pide un  numero al usuario
+numero = int(input("Teclea un numero:"))
+print(f"El numero que se tecleo fue {numero}")
+print("Fin del programa")
+#se agrega un comentario para ver el comando diff de git
