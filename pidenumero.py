8d67ff8dd9e4292af2fa9ed9dd2701e28e1dca96
daniel <danobook@gmail.com>
2021-07-08 18:13:46 -0500

    Se agrego un ciclo while para vovler a pedir el numero si teclea un numero negativo

--- pidenumero.py
+++ pidenumero.py
@@ -1,9 +1,13 @@
 #Se pide un  numero al usuario 
-#En esta version se valida que el numero sea positivo, si se teclea un numeronegativo marca error
-numero = int(input("Teclea un numero positivo:"))
-if numero < 0 :
-    print("Error-> El numero debe ser positivo")
-else:
-    print(f"El numero que se tecleo fue {numero}")
-    print("Fin del programa")
+#En esta version se valida que el numero sea positivo, 
+#si se teclea un numeronegativo marca error y se vuelve a pedir el numero
+while True:
+    numero = int(input("Teclea un numero positivo:"))
+    if numero < 0 :
+        print("Error-> El numero debe ser positivo")
+    else:
+        print(f"El numero que se tecleo fue {numero}")
+        print("Fin del programa")
+        break
+
 
